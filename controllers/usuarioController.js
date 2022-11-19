a08bd74386c510e44e5274a166a91d8841109187
mac <mac@macs-MacBook-Pro.local>
2022-11-19 15:25:38 -0500

    #4 version de email

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,13 +1,45 @@
-const prueba = (req, res)=> {
-    res.send({
-        msg:"En esta ruta gestionaremos todas las peticiones correspondiente al modelo de usuarios"
-    })
+import Usuario from "../models/Usuario.js";
+
+const prueba = (req, res) => {
+  res.send({
+    msg: "En esta ruta gestionaremos todas las peticiones correspondiente al modelo de usuarios",
+  });
 };
 
+const registrar = async (req, res) => {
+
+ 
+
+  const { nombre, email } = req.body;
+  // Validar usuario duplicado
+  // findOne busca por los diferentes atributos de la coleccion
+  const existeUsuario = await Usuario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  };
+  try {
+    const usuario = new Usuario(req.body);
+    const usuarioGuardado = await usuario.save();
+    // Enviar el email
+    emailRegistro({
+      email,
+      nombre,
+      token: usuarioGuardado.token,
+    });
+
+    res.json(usuarioGuardado);
+
+  } catch (error) {
+    console.error(error.message);
+  };
 
 
+};
+const confirmar = (req, res) => {};
 
-
-export {
-    prueba
+export { 
+    prueba, 
+    registrar, 
+    confirmar 
 };
